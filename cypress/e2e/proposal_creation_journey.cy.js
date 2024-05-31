4aa751417ec707f39d7d6494b91b758d46a9f562
Omer Saqib <umersaqib2000@gmail.com>
2024-05-31 16:19:52 +0500

    Proposal Creation Journey changes

--- cypress/e2e/proposal_creation_journey.cy.js
+++ cypress/e2e/proposal_creation_journey.cy.js
@@ -6,5 +6,38 @@ describe("IMPORT", () => {
         return false; 
       });
     });
-    
-});
+    it("Verifies the proposal creation journey", () => {
+      cy.login();
+      cy.get('a svg').should('be.visible');
+      cy.get('a svg').eq(1).click(); 
+      cy.get('div.cursor-pointer.text-primary').eq(1).click();
+      cy.contains("button", "Yes").should("be.visible").click();
+      cy.contains("button", "let's set up the proposal").should("be.visible").click();
+      cy.wait(4000);
+      cy.contains("button", "Select this Template").should("be.visible").click();
+      cy.contains("button", "Proceed with this template").should("be.visible").click();
+      cy.wait(4000);
+      cy.contains("button", "Continue").should("be.visible").click();
+      cy.wait(4000);
+      cy.contains("p", " SUGGEST A RESPONSE TO THIS").should("be.visible").click();
+      cy.wait(25000);
+      cy.contains("button","Next").should("be.visible").click();
+      cy.wait(4000);
+      cy.contains("p", " SUGGEST A RESPONSE TO THIS").should("be.visible").click();
+      cy.wait(25000);
+      cy.contains("button","Next").should("be.visible").click();
+      cy.wait(4000);
+      cy.contains("p", " SUGGEST A RESPONSE TO THIS").should("be.visible").click();
+      cy.wait(25000);
+      cy.contains("button","Next").should("be.visible").click();
+      cy.wait(4000);
+      cy.contains("p", " SUGGEST A RESPONSE TO THIS").should("be.visible").click();
+      cy.wait(25000);
+      cy.contains("button","Next").should("be.visible").click();
+      cy.wait(4000);
+      const randomNumber = Math.floor(Math.random() * 1000); 
+      const proposalName = `OmerAutomationtestproposal${randomNumber}`;
+      cy.get('.gap-6 > .flex-col > .border').type(proposalName)
+      cy.contains('button', "Complete Setup").should('be.visible').click();
+    });
+    });
